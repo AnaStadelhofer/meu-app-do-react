02c277bac8e3fd33676ea4eb923800df4c243eee
Ana <ana.stadelhofer@outlook.com>
2022-09-30 21:51:41 -0300

    alert

--- src/components/Contador.tsx
+++ src/components/Contador.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@mui/material";
-import React, { useState } from "react";
+import { Alert, Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 const Contador = () => {
     const [contador, setContador] = useState(10);
-    const [contadorAux, setContadorAux] = useState(0);
+    const [contadorAux, setContadorAux] = useState(false);
+    const [mensagem, setMensagem] = useState("");
 
     const aumentaMes = () => {
         setContador(contador + 1);
@@ -25,10 +26,23 @@ const Contador = () => {
         setContador(0);
     };
 
+    useEffect(() => {
+        if (contador > 12) {
+          setContadorAux(true);
+          setMensagem("O ano possui apenas 12 meses");
+        } else {
+            setContadorAux(false);
+        }
+    }, [contador]);
+
+
     return(
         <div>
             <h1>Primeiro componente</h1>
             <div>Meu mês de aniversário é: {contador}</div>
+            {contadorAux &&
+                <Alert severity="error">{mensagem}</Alert>
+            }
             <Button onClick={aumentaMes}>Adicionar mês</Button>
             <Button onClick={diminuiMes}>Diminuir mês</Button>
             <Button onClick={multiMes}>Multiplicar mês</Button>
